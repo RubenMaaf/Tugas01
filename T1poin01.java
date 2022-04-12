0ec83b17e716114dda594b9f6ebe8c6c79d603a9
RubenMaaf <103195656+RubenMaaf@users.noreply.github.com>
2022-04-12 18:50:46 +0700

    Setor Tugas 1

---
+++ T1poin01.java
@@ -0,0 +1,28 @@
+
+package t1poin01;
+
+
+public class T1poin01 {
+
+
+    public static void main(String[] args) {
+        
+        {
+        String [] nama={"Ruben","Kalis","Isto","Agus ","Wahyu"};
+  
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        System.out.println(nama[3]);
+        System.out.println(nama[4]);
+        }
+    }
+} 
+        
+        
+        
+        
+
+    
+    
+
